87c524854a759152c0dec00cd1a3b2705ef9460c
Tova Jertfelt <tovizitan@gmail.com>
2022-03-14 08:55:57 +0100

    network error

--- javascript/main.js
+++ javascript/main.js
@@ -1,159 +1,180 @@
-const characterArray = [];
-let dropdownOption1 = document.getElementById("dropdown");
-let dropdownOption2 = document.getElementById("dropdown2");
+// /* Old code - this one worked with a find function on the object, as well as parent and child of Character, but I later on had trouble using the parameters when selecting (transferring from objects to array back to object again) so I started over trying another approach*/ 
 
-class Character{
-  constructor(_name, _gender, _height, _mass, _hairColor, _pictureUrl){
-    this.name = _name;
-    this.gender =_gender;
-    this.height =_height;
-    this.mass = _mass;
-    this.hairColor = _hairColor;
-    this.pictureUrl =_pictureUrl;
+
+// const characterArray = [];
+// let dropdownOption1 = document.getElementById("dropdown");
+// let dropdownOption2 = document.getElementById("dropdown2");
+
+// class Character{
+//   constructor(_name, _gender, _height, _mass, _hairColor, _pictureUrl){
+//     this.name = _name;
+//     this.gender =_gender;
+//     this.height =_height;
+//     this.mass = _mass;
+//     this.hairColor = _hairColor;
+//     this.pictureUrl =_pictureUrl;
  
-  }
+//   }
 
-  async getCharacters(){
-    try {
-      let result = await fetch('https://swapi.dev/api/people/?format=json');
-      let data = await result.json();  
-      let characters = data.results;
-      characters.forEach((item) => {
-       characterArray.push(item);
-      }) 
-      return characters;
-    } catch (error) {
-      console.log(error);
-    }
-  }  
+//   async getCharacters(){
+//     try {
+//       let result = await fetch('https://swapi.dev/api/people/?format=json');
+//       let data = await result.json();  
+//       let characters = data.results;
+//       characters.forEach((item) => {
+//        characterArray.push(item);
+//       }) 
+//       return characters;
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }  
 
+//   weightCompare(item){
+//     if (item.mass > this.mass){
+      
+//     }
+
+//   }
  
-}
+// }
 
-class FirstCharacter extends Character{
-  addingCharactersToSelection() {
-    let userChoice1 = dropdownOption1.value;
-    let userChoice2 = dropdownOption2.value;
-    if (userChoice1 === userChoice2){
-    alert("Please choose two different characters");}
-    const character1 = characterArray.find(x => x.name === userChoice1);
-    return character1;
-    }
+// class FirstCharacter extends Character{
+//   addingCharactersToSelection() {
+//     let userChoice1 = dropdownOption1.value;
+//     let userChoice2 = dropdownOption2.value;
+//     if (userChoice1 === userChoice2){
+//     alert("Please choose two different characters");}
+//     const character1 = characterArray.find(x => x.name === userChoice1);
+//     return character1;
+//     }
 
-  addingCharacter(character1){
-    this.name = character1.name;
-    this.gender = character1.gender;
-    this.height = character1.height;
-    this.mass = character1.mass;
-    this.hairColor = character1.hair_color;
-  }
+//   addingCharacter(character1){
+//     this.name = character1.name;
+//     this.gender = character1.gender;
+//     this.height = character1.height;
+//     this.mass = character1.mass;
+//     this.hairColor = character1.hair_color;
+//   }
 
-  addingPicture(){
-    if (this.name ==="Luke Skywalker") {
-      this.pictureUrl("./img/luke.png")
-    }
+//   addingPicture(){
+//     if (this.name ==="Luke Skywalker") {
+//       this.pictureUrl("./img/luke.png")
+//     }
 
-    if (this.name ==="Biggs Darklighter") {
-      this.pictureUrl("./img/biggs.png")
-    }
+//     if (this.name ==="Biggs Darklighter") {
+//       this.pictureUrl("./img/biggs.png")
+//     }
 
-    if (this.name ==="C-3PO") {
-      this.pictureUrl("./img/c3po.png")
-    }
+//     if (this.name ==="C-3PO") {
+//       this.pictureUrl("./img/c3po.png")
+//     }
 
-    if (this.name ==="Darth vader") {
-      this.pictureUrl("./img/darthvader.png")
-    }
+//     if (this.name ==="Darth vader") {
+//       this.pictureUrl("./img/darthvader.png")
+//     }
 
-    if (this.name ==="Obi-Wan Kenobi") {
-      this.pictureUrl("./img/obiwan.png")
-    }
+//     if (this.name ==="Obi-Wan Kenobi") {
+//       this.pictureUrl("./img/obiwan.png")
+//     }
 
-    if (this.name ==="Owen Lars") {
-      this.pictureUrl("./img/owen.png")
-    }
+//     if (this.name ==="Owen Lars") {
+//       this.pictureUrl("./img/owen.png")
+//     }
 
-    if (this.name ==="Leia Organa") {
-      this.pictureUrl("./img/leila.png")
-    }
+//     if (this.name ==="Leia Organa") {
+//       this.pictureUrl("./img/leila.png")
+//     }
 
-    if (this.name ==="R2-D2") {
-      this.pictureUrl("./img/r2d2.png")
-    }
+//     if (this.name ==="R2-D2") {
+//       this.pictureUrl("./img/r2d2.png")
+//     }
 
-    if (this.name ==="Beru Whitesun lars") {
-      this.pictureUrl("./img/beruwhitesun.png")
-    }
+//     if (this.name ==="Beru Whitesun lars") {
+//       this.pictureUrl("./img/beruwhitesun.png")
+//     }
 
-    if (this.name ==="R5-D4") {
-      this.pictureUrl("./img/r5d4.png")
-    }
-  }
+//     if (this.name ==="R5-D4") {
+//       this.pictureUrl("./img/r5d4.png")
+//     }
+//   }
 
-  // Klassen ska även innehålla metoder för att jämföra karaktärens egna egenskaper med en annan karaktär 
-}
+//   // Klassen ska även innehålla metoder för att jämföra karaktärens egna egenskaper med en annan karaktär 
+// }
 
-class SecondCharacter extends Character{
-  constructor(_name, _id, _gender, _height, _mass, _hairColor, _pictureUrl){
-    super (_pictureUrl);
-    this.name = _name;
-    this.gender =_gender;
-    this.height =_height;
-    this.mass = _mass;
-    this.hairColor = _hairColor;
-    this.pictureUrl =_pictureUrl;
-  }
+// class SecondCharacter extends Character{
+//   constructor(_name, _id, _gender, _height, _mass, _hairColor, _pictureUrl){
+//     super (_pictureUrl);
+//     this.name = _name;
+//     this.gender =_gender;
+//     this.height =_height;
+//     this.mass = _mass;
+//     this.hairColor = _hairColor;
+//     this.pictureUrl =_pictureUrl;
+//   }
 
-  addingSecondCharacter(){
+//   addingSecondCharacter(){
  
-    let userChoice2 = dropdownOption2.value;
+//     let userChoice2 = dropdownOption2.value;
   
 
-    const character2 = characterArray.find(x => x.name === userChoice2);
-    this.name = character2.name;
-    this.gender = character2.gender;
-    this.height = character2.height;
-    this.mass = character2.mass;
-    this.hairColor = character2.hair_color;
+//     const character2 = characterArray.find(x => x.name === userChoice2);
+//     this.name = character2.name;
+//     this.gender = character2.gender;
+//     this.height = character2.height;
+//     this.mass = character2.mass;
+//     this.hairColor = character2.hair_color;
   
-  }
-}
+//   }
+// }
 
-class Creating{
+// class Creating{
 
-  displayCharacter(characters){
+//   displayCharacter(characters){
     
-    let option = "";
-    characters.forEach(character => {
-      option += `
-      <option id="${character.name}" 
-      value="${character.name}">
-      ${character.name}
-      </option>
-      `
-    });
+//     let option = "";
+//     characters.forEach(character => {
+//       option += `
+//       <option id="${character.name}" 
+//       value="${character.name}">
+//       ${character.name}
+//       </option>
+//       `
+//     });
   
-    document.getElementById("dropdown2").innerHTML = option;
-    document.getElementById("dropdown").innerHTML = option; 
-  }
+//     document.getElementById("dropdown2").innerHTML = option;
+//     document.getElementById("dropdown").innerHTML = option; 
+//   }
 
-  displayChosenCharacters(){}
-}
+//   displayChosenCharacters(character1, character2){
+//     const CharacterResult = document.getElementById("inHereTheyShallBe");
 
-//calling:
-  let character = new Character();
-  let creating = new Creating();
-  let firstcharacter = new FirstCharacter();
-  let secondCharacter = new SecondCharacter();
+//     CharacterResult.innerHTML=`
+//     <article>
+//     <h3>Your chosen heroes are:</h3><br>
+//     <p>${character2.name}</p><br>
+//     <p>${character1.name}</p>`
+//   }
+// }
 
-  character.getCharacters().then(characters => {
-    creating.displayCharacter(characters)
-  });
+// //calling:
+//   let character = new Character();
+//   let creating = new Creating();
+//   let firstcharacter = new FirstCharacter();
+//   let secondCharacter = new SecondCharacter();
 
-  document.getElementById("choosingChars").addEventListener("submit", () => { 
-  console.log("") //inserted because of bug
+//   character.getCharacters().then(characters => {
+//     creating.displayCharacter(characters)
+//   });
 
-  character.getCharacters().then(characters => {
-  firstcharacter.addingCharactersToSelection(characters)}).then(secondCharacter.addingSecondCharacter());
-  })
+//   //event listener 
+// document.getElementById("choosingChars").addEventListener("submit", () => { 
+//   console.log("") //inserted because of bug
+//   console.log("tryckt")
+
+//   character.getCharacters().then(characters => {
+//   firstcharacter.addingCharactersToSelection(characters)}).then(secondCharacter.addingSecondCharacter()).then(character1, character2 => { creating.displayChosenCharacters(character1, character2)});
+
+
+// })
 
---
+++ javascript/script.js
@@ -0,0 +1,182 @@
+let characterArray = [];
+let chosenCharacters = [];
+
+//some DOM keys: 
+let dropdown1 = document.getElementById("dropdown");
+let dropdown2 = document.getElementById("dropdown2");
+let renderedDiv = document.getElementById("inHereTheyShallBe");
+
+class FetchingAPI{
+  
+async getCharacters(){
+  try {
+    let result = await fetch('https://swapi.dev/api/people/?format=json');
+    let data = await result.json();  
+    let characters = data.results;
+    characters.forEach((item) => {
+     characterArray.push(item);
+    }) 
+    return characters;
+  } catch (error) {
+    console.log(error);
+  }
+}  
+
+}
+
+class Create {
+  displayCharacterDropDown(characters){
+    
+    let option = "";
+    characters.forEach(character => {
+      option += `
+      <option id="${character.name}" 
+      value="${character.name}">
+      ${character.name}
+      </option>
+      `
+    });
+  
+    document.getElementById("dropdown2").innerHTML = option;
+    document.getElementById("dropdown").innerHTML = option; 
+  }
+  displayChosenCharacters(url,sectionname){
+
+    let headline = document.createElement("h2");
+    headline.innerText("This are the droids you are looking for:")
+    let buttonDiv = document.createElement("div");
+    renderedDiv.appendChild(buttonDiv);
+
+    let characterSectionRendered = document.querySelector(`.${sectionname}`);
+    characterSectionRendered.innerHTML = `<img src="${url}" alt="Star Wars icon"`
+
+    let buttWeight = document.createElement("button");
+    let buttGender = document.createElement("button");
+    let buttHair = document.createElement("button");
+    let buttHeight = document.createElement("button");
+    buttHeight.innerText = "Compare height";
+    buttHair.innerText = "Compare haircolors";
+    buttWeight.innerText = "Compare weight";
+    buttGender.innerText = "Compare genders";
+
+    buttonDiv.classList.add("button--comparisons");
+    buttonDiv.append(
+      buttWeight, 
+      buttHeight,
+      buttHair,
+      buttGender
+    )
+ 
+
+   }
+} 
+
+class Character{
+constructor(_name, _gender, _height, _mass, _haircolor, _pictureUrl){
+  this.name = _name;
+  this.gender = _gender;
+  this.height = _height;
+  this.mass = _mass;
+  this.hairColor = _haircolor;
+  this._pictureUrl = _pictureUrl;
+}
+
+
+
+  compareHeight(item){
+    let myHeight = parseInt(item.height);
+    let otherHeight = parseInt(this.height);
+    if (myHeight > otherHeight){
+      articleElem.innerText = ``;
+    }
+    else {
+      articleElem.innerText = ``;
+    }
+  }
+  compareHair(item){}
+  compareGender(item){}
+  compareWeight(item){
+    if(item.mass >= this.mass){
+      articleElem.innerText = ``;
+    }
+    else {
+      articleElem.innerText = ``
+    }
+  }
+
+
+}
+
+class Comparing extends Character{
+  constructor( _name, _gender, _height, _mass, _hairColor, _pictureUrl){
+    super ( _name, _gender, _height, _mass, _hairColor, _pictureUrl);
+}
+
+}
+
+
+//*--------Loading page
+let character = new Character();
+let creating = new Create();
+let fetching = new FetchingAPI();
+  
+fetching.getCharacters().then(characters => { 
+  creating.displayCharacterDropDown(characters)
+});
+
+
+//*------------Submitting form
+document.getElementById("choosingChars").addEventListener("submit", () => { 
+//inserted because of bug
+  console.log("") 
+  console.log("tryckt")
+
+  //reading the choices:
+  chosenCharacters.length = 0; 
+    renderedDiv.innerHTML ="";
+    let userChosenOne = dropdown1.value;
+    let userChosenTwo = dropdown2.value;
+    if (userChosenOne === userChosenTwo){
+      renderedDiv.innerHTML = `<h3>These are not the droids you are looking for.
+      Please choose two different characters!</h3>`
+    }
+    else {
+      let imgCharacterOne = `./img/${userChosenOne}.png`;
+      let imgCharacterTwo = `./img/${userChosenTwo}.png`;
+      console.log(userChosenOne);
+    
+    //fixing:
+    userChosenOne = userChosenOne.split("-").join(" ");
+    userChosenTwo = userChosenTwo.split("-").join(" ");
+
+    //getting pictures:
+    let characterOne= `https://swapi.dev/api/people?search=${userChosenOne}`
+    console.log("test" + characterOne);
+    let characterTwo = `https://swapi.dev/api/people?search=${userChosenTwo}`
+      getOnlyData(characterOne).then((data) => {
+        let characterConstructor = data.results;
+        console.log(characterConstructor);
+      })
+    //fetch and print:
+    
+
+    
+  
+    }
+})
+
+    //funkade inte när jag hade den i klass (förmodligen för att jag inte  alltid får till hur man pysslar med .then )
+async function getOnlyData (url){
+  let result = await fetch(url);
+  let data = await result.json();
+  return data; }
+
+  // fetching.getCharacters().then(characters => {
+
+
+// document.addEventListener('keydown', function(event){
+// 	if(event.key === "Escape"){
+// 		cartOpacity.classList.remove("transparentBcg");
+//     cartMenu.classList.remove("showCart");
+// 	}
+// });
